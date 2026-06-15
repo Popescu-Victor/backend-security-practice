c8a1c5ed765c15b56c9879489fae10eedd851964
Popescu Victor <romaniahasmetal@gmail.com>
2026-06-15 09:49:16 +0300

    File to filepath object to print filename

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from tkinter import filedialog
 
+# I prefer to have this part in every practice program, so I can run it directly from the terminal while handling files.
+
+
+class FilePath(): # Storing filepath as object for further reference
+    def __init__(self, path):
+        self.path = path
+
+    def get_path(self):
+        return self.path
+
+
 
 def open_file():
     file_path = filedialog.askopenfilename()
     if file_path:
-        print(f"Selected file: {file_path}")
+        file_path_obj = FilePath(file_path)
+        return f"File path object: {file_path_obj.get_path()}"
     else:
-        print("No file selected.")
+        return "No file selected."
 
 if __name__ == "__main__":
-    open_file()
+    filename = open_file().split("/")[-1]
+    print(f"Selected file: {filename}")
